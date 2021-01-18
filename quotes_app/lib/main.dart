e1799112e94a7ddbd1e35bc174fb721ff0a93d5f
loveojha <63944456+loveojha@users.noreply.github.com>
2021-01-18 21:08:50 +0530

    Add files via upload

---
+++ quotes_app/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: QuoteList(),
+  )
+);
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Oscar wilde', text: "Be yourself; everyone else is already taken"),
+    Quote(author: 'Elon Musk', text: "I have nothing to declare except my genious"),
+    Quote(author: 'Jeff Bezos', text: "The truth is rarely pure and never simple"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+        title: Text("Awesome Quotes"),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+        )).toList(),
+      ),
+
+    );
+  }
+}
